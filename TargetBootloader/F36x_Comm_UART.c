4e72b88e204ed99ddfd2f8c9485885298b8e2d0b
Mark <xiaoxid@163.com>
2013-05-13 14:02:16 +0800

    C8051F360 UART bootloader works
    mind difference from F330 is flash page size, SFR page definition. port
    pin.etc

---
+++ TargetBootloader/F36x_Comm_UART.c
@@ -0,0 +1,281 @@
+//-----------------------------------------------------------------------------
+// F36x_Comm_UART.c
+//-----------------------------------------------------------------------------
+// Copyright (C) 2010 Silicon Laboratories, Inc.
+// http://www.silabs.com
+//
+// Program Description:
+//
+// [*** TEMPLATE INFORMATION ***]
+// Filename: Fnnn_Comm_CommType.c
+// nnn => Fill-in with the MCU family part number.
+// CommType => Fill-in Comm interface name.
+// Examples: F36x_Comm_SMB.c, F36x_Comm_UART.c
+//
+//
+//
+//
+// Target:         C8051Fxxx (Any Silicon Labs Flash MCU)
+// Tool chain:     Generic
+// Command Line:   None
+//
+//
+// Release 1.1 / 9Mar2013 (MarkD)
+//    -F360 UART Revision
+// Release 1.0 / 01SEP2010 (PKC)
+//    -Initial Revision
+//
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include <compiler_defs.h>
+#include "Fxxx_SFR_Defs_Wrapper.h"     // This header file will include the
+// real MCU register definition file
+
+#include "Fxxx_Target_Config.h"
+#include "Fxxx_Target_Interface.h"
+
+#include "Fxxx_TargetBL_Config.h"
+#include "Fxxx_TargetBL_Interface.h"
+
+#include "Fxxx_BL129_UART_Interface.h"  
+#include "F36x_CRC.h"
+//-----------------------------------------------------------------------------
+// Global CONSTANTS
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Global Variables
+//-----------------------------------------------------------------------------
+/*
+	infoLength,
+	BLfwVer0,
+	BLfwVer1,
+	MCUcode,
+	BLtype,
+	flashPageSizeCode,
+	BLbufferSizeCode,
+	CRCtype,
+	encryptionType,
+	appFWstartAddr0,
+	appFWstartAddr1,
+	appFWstartAddr2,
+	appFWendAddr0,
+	appFWendAddr1,
+	appFWendAddr2,
+	CANdeviceAddr,
+	appFWver0,
+	appFWver1,
+	*/
+U8 data TGT_BL_InfoBlock[TGT_BL_FW_INFOBLOCK_LENGTH] = 
+{
+	SRC_CMD_DISP_TGT_INFO,
+	TGT_BL_FW_INFOBLOCK_LENGTH,	
+	TGT_BL_FW_VERSION_LOW,
+   	TGT_BL_FW_VERSION_HIGH,
+	TGT_PRODUCT_CODE,
+	TGT_BL_TYPE,
+	TGT_FLASH_PAGE_SIZE_CODE,
+    0xFC, //((TGT_BL_PKT_SIZE_CODE << 4)|(TGT_BL_BUF_SIZE_CODE & 0x0F)),
+    TGT_CRC_TYPE,
+	TGT_ENCRYPTION_TYPE,
+ 	(APP_FW_START_ADDR & 0xFF),
+	((APP_FW_START_ADDR & 0xFF00) >> 8),
+	((APP_FW_START_ADDR & 0xFF0000) >> 16),
+	(APP_FW_END_ADDR & 0xFF),
+	((APP_FW_END_ADDR & 0xFF00) >> 8),
+	((APP_FW_END_ADDR & 0xFF0000) >> 16),
+	0x25,
+	TGT_DEVICE_SERIAL0,
+	TGT_DEVICE_SERIAL1
+};
+extern U8 data rx_buf[32];
+//-----------------------------------------------------------------------------
+// Function Prototypes (Local)
+//-----------------------------------------------------------------------------
+
+//=============================================================================
+// Function Definitions
+//=============================================================================
+//-----------------------------------------------------------------------------
+// SRC_Get_Info
+//-----------------------------------------------------------------------------
+//
+// Return Value:  Response
+// Parameters:    None
+//
+//
+//
+//-----------------------------------------------------------------------------
+U8 SRC_Get_Info(void)
+{
+    U8 data cmd = SRC_CMD_GET_INFO;
+
+    // Command Format:
+    // [0] Command
+	uart_send(&cmd, 1);
+    // Response:
+    // [0] Return code (ACK/ERROR etc)
+    // [1] InfoBlock Length
+    // [2] MCU Code
+    // [3] BL Type
+    // [4] Flash Page Size Code
+    // [5] Flash Key0
+    // [6] Flash Key1
+    // [7] SMB Device Address
+    // [8] Application FW Start Addr Low (0)
+    // [9] Application FW Start Addr (1)
+    // [10] Application FW Start Addr High (2)
+    // [11] Application FW End Addr Low (0)
+    // [12] Application FW End Addr (1)
+    // [13] Application FW End Addr High (2)
+	uart_receive(rx_buf, SRC_CMD_GET_INFO_RX_SZ);
+
+    return (rx_buf[0]);
+}
+
+//-----------------------------------------------------------------------------
+// SRC_Get_Page_Info
+//-----------------------------------------------------------------------------
+//
+// Return Value:  Reponse
+// Parameters:    buf
+//
+//
+//
+//-----------------------------------------------------------------------------
+U8 SRC_Get_Page_Info(void)
+{
+	U8 cmd = SRC_CMD_GET_PAGE_INFO;
+
+    // Command Format:
+    // [0] Command
+	uart_send(&cmd, 1);
+
+    // Response:
+    // [0] Return code (ACK/ERROR etc)
+    // [1] Page address byte 0 {or Num Pages, if return code = SRC_RSP_DATA_END}
+    // [2] Page address byte 1
+    // [3] Page address byte 2 <-- don't care for MCUs 64k and smaller
+    // [4] Page CRC byte 0
+    // [5] Page CRC byte 1
+    // Note: If Num Pages is provided, add 1 to get actual num pages
+	uart_receive(rx_buf, SRC_CMD_GET_PAGE_INFO_RX_SZ);
+
+    return (rx_buf[0]);
+}
+
+//-----------------------------------------------------------------------------
+// SRC_Get_Page
+//-----------------------------------------------------------------------------
+//
+// Return Value:  Resposne
+// Parameters:    buf
+//
+//
+//
+//-----------------------------------------------------------------------------
+U8 SRC_Get_Page(U8 xdata *buf)
+{
+	U8 cmd = SRC_CMD_GET_PAGE;
+    // Command Format:
+    // [0] Command
+	uart_send(&cmd, 1);
+
+    // Response:
+    // [0] Return code (ACK/ERROR etc)
+    // [1..512] Page data
+    // [513] Return code (ACK/ERROR etc)
+	uart_receive(&cmd, 1);
+	uart_receive(buf, SRC_CMD_GET_PAGE_RX_SZ);
+	uart_receive(&cmd, 1);
+
+    return (cmd);
+}
+
+//-----------------------------------------------------------------------------
+// SRC_Disp_TGT_Info
+//-----------------------------------------------------------------------------
+//
+// Return Value:  Response
+// Parameters:    None
+//
+//
+//
+//-----------------------------------------------------------------------------
+U8 SRC_Disp_TGT_Info(void)
+{
+    U8 cmd;
+
+    // Command Format:
+    // [0] Command
+	uart_send(TGT_BL_InfoBlock, TGT_BL_FW_INFOBLOCK_LENGTH);
+
+    // Response:
+    // [0] Return code (ACK/ERROR etc)
+	uart_receive(&cmd, SRC_CMD_DISP_TGT_INFO_RX_SZ);
+
+
+    return (cmd);
+}
+
+//-----------------------------------------------------------------------------
+// SRC_Disp_Info_Code
+//-----------------------------------------------------------------------------
+//
+// Return Value:  None
+// Parameters:    info_code
+//
+//
+//
+//-----------------------------------------------------------------------------
+U8 SRC_Disp_Info_Code(U8 info_code)
+{
+
+    // Command Format:
+    // [0] Command
+    // [1] Info Code
+	rx_buf[0] = SRC_CMD_DISP_INFO_CODE;
+	rx_buf[1] = info_code;
+	uart_send(rx_buf, 2);
+
+    // Response:
+    // [0] Return code (ACK/ERROR etc)
+	uart_receive(rx_buf, SRC_CMD_DISP_INFO_CODE_RX_SZ);
+    return (rx_buf[0]);
+}
+
+//-----------------------------------------------------------------------------
+// Interrupt Service Routines
+//-----------------------------------------------------------------------------
+
+//  uart send, count max number is 255, limited by XRAM size
+void uart_send(U8 data *buf, U8 count)
+{
+	U8 *ptr = buf;
+	do{
+	    SBUF0 = *ptr++;
+		while(TI0 == 0);
+		TI0 = 0;
+	}while(--count);
+}
+
+// uart receive, count max 1024, limited by XRAM size
+void uart_receive(U8 *buf, U16 count)
+{
+	U8 * ptr = buf;
+	do{
+		if(RI0 == 1)
+		{
+			*ptr++ = SBUF0;
+			RI0 = 0;
+			count--;
+		}
+	}while(count);
+}
+
+//-----------------------------------------------------------------------------
+// End Of File
+//-----------------------------------------------------------------------------
---
+++ TargetBootloader/F36x_TargetBL_DevSpecific.c
@@ -0,0 +1,181 @@
+//-----------------------------------------------------------------------------
+// Fnnn_TargetBL_DevSpecific.c
+//-----------------------------------------------------------------------------
+// Copyright (C) 2010 Silicon Laboratories, Inc.
+// http://www.silabs.com
+//
+// Program Description:
+//
+// [*** TEMPLATE INFORMATION ***]
+// Filename: Fnnn_TargetBL_DevSpecific.c
+// nnn => Fill-in with the MCU family part number.
+// Examples: F50x_TargetBL_DevSpecific.c, F33x_TargetBL_DevSpecific.c
+//
+//
+// Target:         C8051Fxxx (Any Silicon Labs Flash MCU)
+// Tool chain:     Generic
+// Command Line:   None
+//
+//
+// Release 1.1 / 9Mar2013 (MarkD)
+//    -F360 UART Revision
+// Release 1.0 / 01SEP2010 (PKC)
+//    -Initial Revision
+//
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include <compiler_defs.h>
+#include "Fxxx_SFR_Defs_Wrapper.h"     // This header file will include the
+// real MCU register definition file
+
+#include "Fxxx_Target_Config.h"
+#include "Fxxx_Target_Interface.h"
+
+#include "Fxxx_TargetBL_Config.h"
+#include "Fxxx_TargetBL_Interface.h"
+
+#include "Fxxx_BL129_UART_Interface.h"
+
+//-----------------------------------------------------------------------------
+// Global CONSTANTS
+//-----------------------------------------------------------------------------
+
+// ------------------------------------------
+// *** Device-Specific *** 
+// Change this as necessary based on MCU
+// ------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Global Variables
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+// Function Prototypes (Local)
+//-----------------------------------------------------------------------------
+void OSCILLATOR_Init(void);
+void PORT_Init(void);
+void UART0_Init(void);
+
+//=============================================================================
+// Function Definitions
+//=============================================================================
+void Disable_Watchdog(void)
+{
+    PCA0MD &= ~0x40; // Disable Watchdog
+}
+
+void Device_Init(void)
+{
+	unsigned char SFRPAGE_save;
+	Disable_Watchdog();
+	
+	SFRPAGE_save = SFRPAGE; // Save the current SFRPAGE
+	SFRPAGE = CONFIG_PAGE;  // Switch to the necessary SFRPAGE
+
+    OSCILLATOR_Init(); // Configure system clock
+    PORT_Init(); // Initialize crossbar
+    UART0_Init(); // Init UART0 & Timer1 (Data source comm)
+	
+	SFRPAGE = SFRPAGE_save;             // Restore the SFRPAGE
+}
+
+//-----------------------------------------------------------------------------
+// OSCILLATOR_Init
+//-----------------------------------------------------------------------------
+//
+// Return Value : None
+// Parameters   : None
+//
+// This routine initializes the system clock to use the internal oscillator
+// at its maximum frequency.
+// Also enables the Missing Clock Detector.
+//-----------------------------------------------------------------------------
+void OSCILLATOR_Init(void)
+{
+
+    OSCICN = 0x83; // Set system clock to 24.5 MHz
+    VDM0CN = 0x80; // Enable VDD monitor and early warning
+    RSTSRC = 0x02; // Enable VDD monitor as a reset source
+}
+
+//-----------------------------------------------------------------------------
+// PORT_Init
+//-----------------------------------------------------------------------------
+//
+// Return Value : None
+// Parameters   : None
+//
+// Configure the Crossbar and GPIO ports.
+//
+// P0.1   digital   push-pull    UART0 TX
+// P0.2   digital   open-drain   UART0 RX
+//
+// P3.2   digital  push-pull     LED
+// P3.0   digital  open-drain    SW
+
+//-----------------------------------------------------------------------------
+void PORT_Init(void)
+{
+    // Initialize port I/O
+    P0MDOUT = 0x02; // Enable UART0 TX as push-pull output
+    P3MDOUT = 0x04; // P3.2 (LED) is push-pull
+//    P0SKIP = 0x03;
+    XBR0 = 0x01; // Enable UART0
+    XBR1 = 0x40; // Enable crossbar
+}
+
+
+//-----------------------------------------------------------------------------
+// UART0_Init
+//-----------------------------------------------------------------------------
+//
+// Return Value : None
+// Parameters   : None
+//
+// Configure the UART0 using Timer1, for <BAUDRATE> and 8-N-1.
+//-----------------------------------------------------------------------------
+void UART0_Init(void)
+{
+    SCON0 = 0x10; // SCON0: 8-bit variable bit rate
+    //        level of STOP bit is ignored
+    //        RX enabled
+    //        ninth bits are zeros
+    //        clear RI0 and TI0 bits
+    if (SYSCLK / BAUDRATE / 2 / 256 < 1)
+    {
+        TH1 = -(SYSCLK / BAUDRATE / 2); // 0x96
+        CKCON &= ~0x0B; // T1M = 1; SCA1:0 = xx
+        CKCON |= 0x08;
+    }
+    else if (SYSCLK / BAUDRATE / 2 / 256 < 4)
+    {
+        TH1 = -(SYSCLK / BAUDRATE / 2 / 4);
+        CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 01
+        CKCON |= 0x01;
+    }
+    else if (SYSCLK / BAUDRATE / 2 / 256 < 12)
+    {
+        TH1 = -(SYSCLK / BAUDRATE / 2 / 12);
+        CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 00
+    }
+    else
+    {
+        TH1 = -(SYSCLK / BAUDRATE / 2 / 48);
+        CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 10
+        CKCON |= 0x02;
+    }
+
+    TL1 = TH1; // init Timer1
+    TMOD &= ~0xf0; // TMOD: timer 1 in 8-bit autoreload
+    TMOD |= 0x20;
+    TR1 = 1; // START Timer1
+}
+
+
+//-----------------------------------------------------------------------------
+// End Of File
+//-----------------------------------------------------------------------------
